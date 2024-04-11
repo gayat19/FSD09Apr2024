a620fb4ebaae9a85d18e0fb06e0bffd7b1514177
Gayathri Mahadevan <gayat_19@yahoo.com>
2024-04-11 16:39:43 +0530

    Day3

---
+++ Day3/ApplicationSolution/FirstApp/Employee.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstApp
+{
+    internal class Employee
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Another");
+        }
+    }
+}
---
+++ Day3/ApplicationSolution/FirstApp/Program.cs
@@ -0,0 +1,43 @@
+﻿using System.Net.Http.Headers;
+
+namespace FirstApp
+{
+    internal class Program
+    {
+        static int Add(int num1,int num2)
+        {
+            checked
+            {
+                int result = num1 + num2;
+                return result;
+            }
+        }
+        static int TakeNumber()
+        {
+            int num1;
+            Console.WriteLine("Please enter the the number");
+            while(!int.TryParse(Console.ReadLine(),out num1))
+                Console.WriteLine("Invalid entry. Please enter a number");
+            return num1;
+        }
+        static void Calculate()
+        {
+            int num1, num2;
+            num1 = TakeNumber();
+            num2 = TakeNumber();
+            int sum = Add(num1,num2);
+            PrintResult(sum,"Sum");
+        }
+
+        static void PrintResult(int sum, string ops)
+        {
+            Console.WriteLine($"The {ops} is {sum}");
+        }
+
+        static void Main(string[] args)
+        {
+            Calculate();
+        }
+    }
+ 
+}
